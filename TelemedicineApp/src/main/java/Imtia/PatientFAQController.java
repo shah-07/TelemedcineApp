04ddb005ca43098eda1921e59c315dc50c205d06
imtia66 <imtiaamirnisha@gmail.com>
2025-07-31 17:08:55 +0600

    Added

---
+++ TelemedicineApp/src/main/java/Imtia/PatientFAQController.java
@@ -0,0 +1,32 @@
+package Imtia;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class PatientFAQController
+{
+    @javafx.fxml.FXML
+    private TextField howDoTF;
+    @javafx.fxml.FXML
+    private TextField canIResTF;
+    @javafx.fxml.FXML
+    private TextField whereCanTF;
+    @javafx.fxml.FXML
+    private TextField askQuestionTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void canIResOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void howDoOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void whereCanOA(ActionEvent actionEvent) {
+    }
+}
