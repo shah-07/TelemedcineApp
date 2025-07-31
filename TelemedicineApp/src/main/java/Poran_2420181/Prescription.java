e77817632607cb2368650210ff4601aea64f13dc
shah-07 <shahporanabdullah.a@gmail.com>
2025-07-31 23:18:42 +0600

    Poran commit

---
+++ TelemedicineApp/src/main/java/Poran_2420181/Prescription.java
@@ -0,0 +1,34 @@
+package Poran_2420181;
+
+public class Prescription {
+    private final String name, gender;
+    private final int age;
+    private String medicines;
+
+    public Prescription(String name, String gender, int age, String medicines) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.medicines = medicines;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMedicines() {
+        return medicines;
+    }
+
+    public void setMedicines(String medicines) {
+        this.medicines = medicines;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
