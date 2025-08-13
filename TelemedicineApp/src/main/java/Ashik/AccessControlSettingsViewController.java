d5a7fa246980b3cc20bea24b4dab5c288927a756
ashik-tac <asjashik715@gmail.com>
2025-08-13 16:31:10 +0600

    work in progres

--- TelemedicineApp/src/main/java/Ashik/AccessControlSettingsViewController.java
+++ TelemedicineApp/src/main/java/Ashik/AccessControlSettingsViewController.java
@@ -1,7 +1,13 @@
 package Ashik;
 
+import javafx.event.ActionEvent;
+
 public class AccessControlSettingsViewController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    public void saveChangesButton(ActionEvent actionEvent) {
+    }
+}
--- TelemedicineApp/src/main/java/Ashik/BackupAndRestoreData.java
+++
@@ -1,7 +0,0 @@
-package Ashik;
-
-public class BackupAndRestoreData
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
